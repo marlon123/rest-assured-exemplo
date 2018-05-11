f77b4aad032ac414e398fc24e84700dfc8172c47
Marlon Lopes Pinto <marlon.pinto@tqi.com.br>
2018-05-11 17:43:21 -0300

    metodo get

--- src/test/java/apitest/TestesAPI.java
+++ src/test/java/apitest/TestesAPI.java
@@ -13,16 +13,33 @@ public class TestesAPI {
 	
 	@Test
     /*Chama o serviço pelo metodo POST*/
-    public void testCriaUsuario() {
+    public void testImprimiJson() {
         String myJson = "{\"Teste\"}";
-         
+         /*Dado uma URL (o método Given entende que a URL a ser chamada
+          *  é à definida no construtor da classe) passando no body um JSON
+          */
          given()
            .contentType("application/json")
            .body(myJson)
+           //quando (when) for por meio do método POST,
          .when()
            .post("/")
+           //o resultado esperado é que o status de retorno seja igual a 200
          .then()
            .statusCode(200);    
     }
+	
+	@Test
+	/*Chama o serviço pelo método GET*/
+	public void testRequisicaoGet() {
+		/*Dado uma URL, quando a requisição for feita usando o 
+		 * tipo GET, passando o parâmetro 1
+		 */
+		given()
+		.when()
+			.get("/123")
+		.then()
+			.statusCode(200);
+	}
 
 }
